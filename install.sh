8c1c37fb118d3fa3d9a714596976a2fd298b32c6
econde <ezequiel.conde@gmail.com>
2023-01-24 18:42:32 +0000

    Receber como argumento a diretoria de instalação

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-base_dir=/usr/local
+if [ $# -ne 1 ]; then
+	echo "usage: $0 <install directory>"
+	exit
+fi
+
+base_dir=$1
 
 if [ ! -d $base_dir/lib/pkgconfig ]; then
 	mkdir $base_dir/lib/pkgconfig
